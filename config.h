c74560b1f14023bf43c9d01a808d56990bc439f8
Malte Dömer <maltedoemer@gmail.com>
2022-06-24 19:55:22 +0200

    updated config

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 5, .gappx = 5};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "fontawesome:size=12" };
+static const char dmenufont[]       = "fontawesome:size=12";
 
 /* background color */
 static const char col_gray1[]       = "#333333";
